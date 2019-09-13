451a17b1a8b03bfc7ffb4cd67f3049fd33b54b53
freeonisland <freeonisland@yahoo.fr>
2019-09-13 16:24:11 +0200

    mbstring

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 error_reporting(E_ALL);
 ini_set('display_errors',1);
 
+/*IntlChar::chr(0x26b6).'<br/>'.
+mb_convert_encoding("\x26\xb7", 'UTF-8', 'UTF-16') .'<br/>'.
+"\u{1f600}".'<br/>';*/
+
 include 'app/autoload.php';
 
 new Classes\App();
