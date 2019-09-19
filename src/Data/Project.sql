070c53a68319d4b35d6464d7afb28b3fa07d0418
freeonisland <freeonisland@yahoo.fr>
2019-09-19 10:39:37 +0200

    add project sql

---
+++ src/Data/Project.sql
@@ -0,0 +1,16 @@
+-- MySql
+USE crowd;
+
+CREATE TABLE IF NOT EXISTS `crowd`.`project` (
+  `id_project` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `fk_id_user` int(11) NOT NULL,
+  PRIMARY KEY (`id_project`),
+  KEY `FK_ID_USER` (`fk_id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `crowd`.`project` 
+ADD CONSTRAINT `Fk_id_user` FOREIGN KEY (`fk_id_user`) 
+REFERENCES `user`(`id`) 
+ON DELETE RESTRICT ON UPDATE RESTRICT;
