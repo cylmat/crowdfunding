9094dab8b145ad659d292871aa121b1a11c2c045
freeonisland <freeonisland@yahoo.fr>
2019-10-15 09:23:09 +0200

    session

---
+++ src/Classes/Session.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types = 1);
+
+namespace Classes;
+
+class Session
+{
+    static function start(): void
+    {
+        if(!isset($_SESSION)) {
+            session_start();
+        }
+    }
+
+    static function destroy(): void
+    {
+        unset($_SESSION);
+        session_destroy();
+    }
+
+    static function set(string $key, $value): void
+    {
+        self::start();
+        $_SESSION[$key] = $value;
+    }
+
+    static function get(string $key)
+    {
+        self::start();
+        if(!empty($_SESSION[$key])) {
+            return $_SESSION[$key];
+        }
+    }
+}
