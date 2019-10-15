c28a92846c8055da7767487abf089a9f7e3968f5
freeonisland <freeonisland@yahoo.fr>
2019-10-15 09:57:23 +0200

    clear repo

--- src/Ctrl/Layout.php
+++ src/Ctrl/Layout.php
@@ -21,8 +21,12 @@ class Layout
 
     function footerAction()
     {
+        $user = Session::get('id_user');
+        $logged = null!==$user ? true : false;
+
         return [
-            
+            'user' => $user,
+            'logged' => $logged
         ];
     }
 }
